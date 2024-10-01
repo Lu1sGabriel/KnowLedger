21adb04611a3e4a7648a075f4a6237ec08910c9c
Lu1sGabriel <luisgabriel533@gmail.com>
2024-09-30 21:04:54 -0300

    Set up initial project structure

---
+++ src/test/java/com/knowledger/knowledger/KnowledgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowledger.knowledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
