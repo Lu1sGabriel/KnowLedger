df347c753ff731e406f72e58b9cce0e90243c4fd
Diogo de Aguiar <Diogobarreto2004@gmail.com>
2024-10-01 23:07:48 -0300

    add: Like, Tag entity e repository

--- src/main/java/com/knowledger/knowledger/infra/persistence/post/IPostRepository.java
+++ src/main/java/com/knowledger/knowledger/infra/persistence/post/IPostRepository.java
@@ -1,4 +1,10 @@
 package com.knowledger.knowledger.infra.persistence.post;
 
-public interface IPostRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface IPostRepository extends JpaRepository<PostEntity, UUID> {
 }
--- src/main/java/com/knowledger/knowledger/infra/persistence/tag/TagEntity.java
+++ src/main/java/com/knowledger/knowledger/infra/persistence/tag/TagEntity.java
@@ -1,4 +1,19 @@
 package com.knowledger.knowledger.infra.persistence.tag;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tag")
 public class TagEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", length = 300)
+    private String description;
+
 }
