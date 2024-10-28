320bbee8ca3db1648eac98c971f9b2ed36b51072
Lu1sGabriel <luisgabriel533@gmail.com>
2024-10-28 00:58:32 -0300

    fix: Removed JavaDocs, removed substring :(, removed loggers. Readjusted BEARER_PREFIX constant.

--- src/main/java/com/knowledger/knowledger/infra/config/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/knowledger/knowledger/infra/config/security/CustomAuthenticationEntryPoint.java
@@ -1,53 +1,30 @@
 package com.knowledger.knowledger.infra.config.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.knowledger.knowledger.infra.exceptions.BusinessExceptionDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
-/**
- * Ponto de entrada personalizado para autenticação, utilizado pelo Spring Security para lidar com tentativas
- * de acesso não autorizadas a recursos protegidos. Este componente fornece uma resposta de erro estruturada
- * em JSON e registra um log sempre que uma tentativa de autenticação falha.
- */
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CustomAuthenticationEntryPoint.class);
     private static final String UNAUTHORIZED_MESSAGE = "Acesso não autorizado.";
 
-    /**
-     * Método invocado automaticamente pelo Spring Security sempre que uma tentativa de acesso a um recurso protegido
-     * falha por falta de autenticação. Este método configura o código de status HTTP como 401 (Unauthorized),
-     * define o tipo de conteúdo da resposta como JSON, e envia uma mensagem de erro estruturada.
-     *
-     * @param request       a solicitação HTTP recebida pelo servidor.
-     * @param response      a resposta HTTP que será enviada ao cliente, contendo o status de erro e a mensagem de erro.
-     * @param authException a exceção de autenticação lançada pelo Spring Security, indicando a causa da falha de autenticação.
-     * @throws IOException se ocorrer um erro ao escrever a resposta JSON no corpo da resposta HTTP.
-     */
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        LOGGER.warn("Tentativa de acesso não autorizado: {}", authException.getMessage());
-
+        response.setContentType("application/json;charset=UTF-8");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType("application/json");
 
-        Map<String, String> data = new HashMap<>();
-        data.put("error", UNAUTHORIZED_MESSAGE);
+        BusinessExceptionDTO errorResponse = new BusinessExceptionDTO(UNAUTHORIZED_MESSAGE);
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonResponse = mapper.writeValueAsString(errorResponse);
 
-        var out = response.getOutputStream();
-        var mapper = new ObjectMapper();
-        mapper.writeValue(out, data);
-        out.flush();
+        response.getWriter().write(jsonResponse);
     }
 
 }
--- src/main/java/com/knowledger/knowledger/infra/config/security/SecurityConfig.java
+++ src/main/java/com/knowledger/knowledger/infra/config/security/SecurityConfig.java
@@ -15,11 +15,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-/**
- * Configuração de segurança da aplicação utilizando o Spring Security.
- * Define as políticas de acesso aos endpoints, o gerenciamento de sessão como stateless,
- * e configura os filtros de autenticação para uso de JWT.
- */
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -28,27 +23,11 @@ public class SecurityConfig {
     private final SecurityFilter _securityFilter;
     private final CustomAuthenticationEntryPoint _authenticationEntryPoint;
 
-    /**
-     * Construtor da configuração de segurança.
-     *
-     * @param securityFilter           filtro de segurança personalizado para autenticação com JWT.
-     * @param authenticationEntryPoint ponto de entrada personalizado para tratamento de acessos não autorizados.
-     */
     public SecurityConfig(SecurityFilter securityFilter, CustomAuthenticationEntryPoint authenticationEntryPoint) {
         _securityFilter = securityFilter;
         _authenticationEntryPoint = authenticationEntryPoint;
     }
 
-    /**
-     * Configura a cadeia de filtros de segurança da aplicação.
-     * Define as permissões de acesso para os endpoints, desabilita a proteção CSRF (por ser uma API stateless),
-     * configura o gerenciamento de sessão como stateless, e adiciona o filtro de segurança personalizado antes
-     * do filtro de autenticação padrão.
-     *
-     * @param http o objeto {@link HttpSecurity} utilizado para configurar a segurança HTTP da aplicação.
-     * @return o {@link SecurityFilterChain} configurado com as definições de segurança.
-     * @throws Exception caso ocorra algum erro durante a configuração da segurança.
-     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -64,26 +43,11 @@ public class SecurityConfig {
         return http.build();
     }
 
-    /**
-     * Configura o gerenciador de autenticação da aplicação, permitindo o acesso ao {@link AuthenticationManager}.
-     * O gerenciador de autenticação é responsável por processar as credenciais de autenticação dos usuários.
-     *
-     * @param authenticationConfiguration a configuração de autenticação do Spring Security, usada para obter o gerenciador de autenticação.
-     * @return uma instância de {@link AuthenticationManager} configurada para a aplicação.
-     * @throws Exception caso ocorra algum erro ao obter o {@link AuthenticationManager}.
-     */
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-    /**
-     * Configura o codificador de senha utilizado pela aplicação.
-     * Utiliza o algoritmo BCrypt para codificar as senhas, garantindo segurança na sua persistência.
-     * O BCrypt é amplamente utilizado devido à sua resistência a ataques de força bruta.
-     *
-     * @return uma instância de {@link PasswordEncoder} configurada com o algoritmo BCrypt.
-     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
