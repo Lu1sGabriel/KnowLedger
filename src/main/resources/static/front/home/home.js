393634868d7329dcef74d77164bdd7362ed7f34e
cunhatalisca <gabrielvlog11@gmail.com>
2024-11-11 20:43:15 -0300

    feat: add render posts

--- src/main/resources/static/front/home/home.js
+++ src/main/resources/static/front/home/home.js
@@ -1,22 +1,51 @@
-import httpService from "../../services/public/httpService.js";
 
 
-document.addEventListener("DOMContentLoaded", async function () {
-    const authToken = localStorage.getItem("authToken");
-
-    if (!authToken) {
-        // Redireciona para a página de login se o authToken não existir
-        window.location.href = "../users/public/login/login.html";
-    }
-
-    const apiUrl = "/api/posts";
-
-
+async function fetchPosts() {
+    const token = localStorage.getItem('authToken'); 
+    console.log(token)
     try {
-        const posts = await httpService.get(apiUrl);
-        console.log(posts);
+        const response = await fetch('http://localhost:8080/post-types/getAll', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`Erro: ${response.status} - ${response.statusText}`);
+            return;
+        }
+
+        const data = await response.json();
+        renderPosts(data);
+        console.log(data); 
     } catch (error) {
-        console.error("Erro ao carregar os posts:", error);
+        console.error('erro ao buscar post ', error);
+    }
+}
+
+
+function renderPosts(posts) {
+    const postsContainer = document.getElementById('.section-news');
+    postsContainer.innerHTML = '';  
+
+    if (posts.length === 0) {
+        postsContainer.innerHTML = '<p>Nenhum post encontrado.</p>';
+        return;
     }
 
-}   );
+    newsSection.innerHTML = '';
+
+    posts.forEach(post => {
+        const postElement = document.createElement('div');
+        postElement.classList.add('post');
+        postElement.innerHTML = `
+            <h2>${post.title}</h2>
+            <p>${post.content}</p>
+        `;
+        postsContainer.appendChild(postElement);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', fetchPosts);
