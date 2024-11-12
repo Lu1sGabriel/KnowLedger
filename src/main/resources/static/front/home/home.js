ebc28ee868fff1bb61fe01905847eee7a79a64b6
cunhatalisca <gabrielvlog11@gmail.com>
2024-11-12 18:40:02 -0300

    feat: consumindo posts vindo do backend

--- src/main/resources/static/front/home/home.js
+++ src/main/resources/static/front/home/home.js
@@ -1,28 +1,49 @@
+document.addEventListener('DOMContentLoaded', async function() {
+    async function fetchPosts() {
+        const token = localStorage.getItem('authToken'); 
+        console.log(token);
+        try {
+            const response = await fetch('http://localhost:8080/post-types/getAll', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+            });
 
+            if (!response.ok) {
+                throw new Error(`Erro: ${response.status} - ${response.statusText}`);
+            }
 
-async function fetchPosts() {
-    const token = localStorage.getItem('authToken'); 
-    console.log(token)
-    try {
-        const response = await fetch('http://localhost:8080/post-types/getAll', {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json',
-            },
-        });
+            const data = await response.json();
+            console.log(data); 
 
-        if (!response.ok) {
-            throw new Error(`Erro: ${response.status} - ${response.statusText}`);
+            renderPosts(data);
+        } catch (error) {
+            console.error('Erro ao buscar posts: ', error);
+        }
+    }
+
+    function renderPosts(posts) {
+        const postsContainer = document.querySelector('.card-custom')
+        if (!postsContainer) {
+            console.error('Elemento card não encontrado');
             return;
         }
 
-        const data = await response.json();
-        renderPosts(data);
-        console.log(data); 
-    } catch (error) {
-        console.error('erro ao buscar post ', error);
+        postsContainer.innerHTML = ''; // Limpa o container antes de inserir novos posts
+
+        posts.forEach(post => {
+            const postElement = document.createElement('div');
+            postElement.classList.add('card', 'mb-3', 'p-3');
+
+            postElement.innerHTML = `
+                <p>${post.description}</p>
+            `;
+
+            postsContainer.appendChild(postElement);
+        });
     }
-}
 
-
+    fetchPosts();
+});
